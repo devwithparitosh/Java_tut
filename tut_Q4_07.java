538c5d87794b1f0cbe7c23e0dd3cd7cbc1e696c4
devwithparitosh <paritoshkum22+git@gmail.com>
2025-02-26 19:49:25 +0530

    j6

---
+++ tut_Q4_07.java
@@ -0,0 +1,15 @@
+/*Q4:- WAP a program to convert kilometer to miles.*/
+import java.util.Scanner;
+
+public class tut_Q4_07 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the Kilometer: ");
+        float k=sc.nextFloat();
+
+        float miles=k/1.609344f;
+
+        System.out.println(k+" Kilometer is equal to "+ miles+" miles.");
+    }
+}
