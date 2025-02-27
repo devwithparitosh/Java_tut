73483bd171a0a17dbbb2f67ba78d23671e835990
devwithparitosh <paritoshkum22+git@gmail.com>
2025-02-27 20:39:37 +0530

    j811

--- Precdence_assositivity_09.java
+++ Precdence_assositivity_09.java
@@ -3,13 +3,13 @@
 public class Precdence_assositivity_09 {
     public static void main(String[] args){
         int a=6*5-34/2;
-/*        Precedence :-in precedence we look for the high op
+/*        Precedence :-in precedence we look for the high precedence operator
           =30-34/2
           =30-17
           =13                  */
         System.out.println(a);
 
-//        Associative  :- in this term we look for left to right associativity
+//        Associative  :- in this term we look for left to right associativity of the operator in case of precedence unable to apply
         int b=60/5-34*2;
 //        =12-34*2
 //        =12-68
