38e2780d94dfe72bb8f5c2182f401f07e2780aa0
devwithparitosh <paritoshkum22+git@gmail.com>
2025-03-01 20:51:50 +0530

    j124

---
+++ Pra_Q4_12.java
@@ -0,0 +1,13 @@
+//Que4:-WAP to foolowing expression:-v2-u2/2as
+
+
+public class Pra_Q4_12 {
+    public static void main(String[] args){
+        int u=2,v=4,a=5,s=1;
+
+        float exp=((v*v)-(u*u));
+        float exp2=exp/(2*a*s);
+
+        System.out.println(exp2);
+    }
+}
