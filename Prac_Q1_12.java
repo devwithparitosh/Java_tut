8e547e25b0ac82c8b14e8dbe917e418347a743b8
devwithparitosh <paritoshkum22+git@gmail.com>
2025-03-01 19:53:12 +0530

    j12

---
+++ Prac_Q1_12.java
@@ -0,0 +1,9 @@
+//Que:-What will be the result of the following expression
+//float a=7/4*9/2
+
+public class Prac_Q1_12 {
+    public static void main(String[] args){
+        float a=7/4 * 9/2;
+        System.out.println(a);
+    }
+}
