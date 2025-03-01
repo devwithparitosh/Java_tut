ad197043d6000a5cf017e60936d39b1de74d7895
devwithparitosh <paritoshkum22+git@gmail.com>
2025-03-01 21:01:46 +0530

    pracQ5_12

---
+++ Pra_Q5_12.java
@@ -0,0 +1,10 @@
+//Que5:-WAP for the following expression :- int a=7*49/7+35/7
+
+public class Pra_Q5_12 {
+    public static void main(String[] args){
+        int x=7;  //given in the que
+
+        int a=((7*49)/7)+(35/7);  //Expression
+        System.out.println(a);
+    }
+}
