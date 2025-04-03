0fdf09012fdbb2c409d4ff459c98075b8b4501db
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-03 12:17:35 +0530

    c2

--- Prac_Q5_15.java
+++ Prac_Q5_15.java
@@ -4,6 +4,7 @@ import java.sql.SQLOutput;
 //letter ="Dear Shadow Monarch, This is your Army"
 public class Prac_Q5_15 {
     public static void main(String[] args){
+
         String letter ="Dear Shadow Monarch,\n\tThis is your Army.\nArise!!";
         System.out.println(letter);
 
