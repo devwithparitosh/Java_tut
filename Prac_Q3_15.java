773efee5189f1b43eb4b1beac988d7acf794e01a
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-03 11:50:49 +0530

    prast

---
+++ Prac_Q3_15.java
@@ -0,0 +1,9 @@
+//Q:-WAP a java program to fill in a letter template which books like below :
+
+public class Prac_Q3_15 {
+    public static void main(String[] args){
+        String letter="Dear <|name|>,Thanks a lot ";
+        System.out.println(letter.replace("<|name|>","Warlord"));
+    }
+}
+
