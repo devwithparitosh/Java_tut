4c4322a413d9b6dfc2a60a20eee5ffe9036eb685
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-03 12:15:49 +0530

    c1

---
+++ Prac_Q5_15.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+//Q:- Write a program to format the following letter using esacpe sequence charaters
+//letter ="Dear Shadow Monarch, This is your Army"
+public class Prac_Q5_15 {
+    public static void main(String[] args){
+        String letter ="Dear Shadow Monarch,\n\tThis is your Army.\nArise!!";
+        System.out.println(letter);
+
+    }
+}
