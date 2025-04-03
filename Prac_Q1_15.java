834fe2c64c393dfd2031f04e71b873aa45e15a0b
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-03 11:39:25 +0530

    prastr1

---
+++ Prac_Q1_15.java
@@ -0,0 +1,11 @@
+//Q:-WAP a java program to convert  a string to lowercase
+
+public class Prac_Q1_15 {
+    public static void main(String[] args){
+
+        String name="Harry POtter";
+        System.out.println("before in lowercase "+name);
+        System.out.println("After lowercase "+name.toLowerCase());
+
+    }
+}
