1b97be8b8c943ec8abab31789e0d447d9cb8529a
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-03 11:43:46 +0530

    prastr

---
+++ Prac_Q2_15.java
@@ -0,0 +1,8 @@
+//Q:- WAP a java program to replace spaces with Underscores
+
+public class Prac_Q2_15 {
+    public static void main(String[] args){
+        String str="You are Shadow King";
+        System.out.println(str.replaceAll(" ","_"));
+    }
+}
