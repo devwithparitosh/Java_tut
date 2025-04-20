b6e970ed22b5dc64f722f5a983b3902c785d5ca7
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-20 20:18:05 +0530

    PracQue_01_25

---
+++ PracQue_01_25.java
@@ -0,0 +1,12 @@
+public class PracQue_01_25 {
+    public static void main(String[] args){
+        int num=5;
+        for (int i=1;i<=num;i++){
+            for (int j=1;j<=num-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
