fe3480a72e23eaa6e890829096e053af26387d9a
devwithparitosh <paritoshkum22+git@gmail.com>
2025-04-27 23:59:11 +0530

    AccessModifier_40_2

--- AccessModifier_40.java
+++ AccessModifier_40.java
@@ -1,7 +1,7 @@
 //public  -->	The code is accessible for all classes
 //private  -->	The code is only accessible within the declared class
 //default  -->	The code is only accessible in the same package. This is used when you don't specify a modifier. You will learn more about packages in the Packages chapter
-//protected--->	The code is accessible in the same package and subclasses. You will learn more about subclasses and superclasses in the Inheritance chapter
+//protected--->	The code is accessible in the same package and subclasses.
 
 class Myemp{
     private int id;
