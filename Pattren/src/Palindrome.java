f8bc67b05b119b298c4912fab46c6ebc3223cd0c
devwithparitosh <paritoshku22+git@gmail.com>
2025-06-22 17:24:30 +0530

    am1

---
+++ Pattren/src/Palindrome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("Enter the number to Check it's Palindrome or not :");
+//        int para= sc.;
+//
+//        String []num=new String[para];
+
+
+    }
+}
