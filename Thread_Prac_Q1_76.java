b49c4ec1d8f377d4b84004445ed895e45d0d4c86
devwithparitosh <paritoshku22+git@gmail.com>
2025-06-26 20:01:25 +0530

    Thread_PraQ1

--- Thread_Prac_Q1_76.java
+++ Thread_Prac_Q1_76.java
@@ -1,2 +1,23 @@
+//Q1:-WAP to print "good morning " and "welcome" continuously on the screen in java using Threads
+
+class greet extends Thread{
+    public greet(String name){
+        super(name);
+    }
+    public void run(){
+        int i=0;
+        while(i<=100){
+            System.out.println(Thread.currentThread().getName());
+            i++;
+        }
+    }
+}
 public class Thread_Prac_Q1_76 {
+    public static void main(String[] args){
+        greet t1=new greet("Good morning");
+        greet t2=new greet("Welcome");
+
+        t1.start();
+        t2.start();
+    }
 }
