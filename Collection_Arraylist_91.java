0563b5a239470543f06e7484cf04e4a4aebf82c8
devwithparitosh <paritoshku22+git@gmail.com>
2025-06-27 20:31:22 +0530

    Collection_Arraylist_91

---
+++ Collection_Arraylist_91.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Collection_Arraylist_91 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+
+        ArrayList<Integer> list2=new ArrayList<>();
+
+        list2.add(12);
+        list2.add(65);
+        list2.add(90);
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(8);
+
+        list.add(0,82);            //it take  the position of the value but not replace it(index,value )
+
+        list.addAll(list2);         //it add the list 2 value in list 1 at the end of the list 1.
+
+        //list.addAll(0,list2);        //(index , list value you want to add)
+
+        //list.clear();                 //it clear all the value.
+
+        System.out.println(list.contains(5));
+
+        System.out.println(list.indexOf(12));
+
+        list.remove(0);   //it remove the value at particular index
+
+        list.set(0,2);          //it replace the value at the index and Set the given value.
+
+        System.out.println(list.get(4));
+
+        for (int i=0;i<list.size();i++){
+
+            System.out.print(list.get(i));       //get method is used to access the array index
+            System.out.print(", ");
+
+        }
+    }
+}
---
+++ Collection_framework_88.java
@@ -0,0 +1,9 @@
+//The Java Collections Framework provides a set of interfaces (like List, Set, and Map) and a set of classes (ArrayList, HashSet, HashMap, etc.) that implement those interfaces.
+//
+//All of these are part of the java.util package.
+
+public class Collection_framework_88 {
+    public static void main(String[] args) {
+
+    }
+}
